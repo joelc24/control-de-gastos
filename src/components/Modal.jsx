d45b13b0020c7c21b5ee8b0699bbf2092db131cd
Joel oficina Cues <libra-marriaga@hotmail.com>
2022-03-07 13:08:57 -0500

    añadiendo el formulario de gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -21,6 +21,42 @@ const Modal = ({setModal,animarModal,setAnimarModal}) => {
             </div>
             <form className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
                 <legend>Nuevo Gasto</legend>
+                <div className="campo">
+                    <label htmlFor="nombre">Nombre Gasto</label>
+                    <input 
+                        type="text" 
+                        placeholder='Añade el Nombre del Gasto'
+                        id='nombre'
+                    />
+                </div>
+                <div className="campo">
+                    <label htmlFor="cantidad">Cantidad</label>
+                    <input 
+                        type="number" 
+                        placeholder='Añade la Cantidad del Gasto: ej 300'
+                        id='nombre'
+                    />
+                </div>
+                <div className="campo">
+                    <label htmlFor="categoria">Categoria</label>
+                    <select 
+                        name="" 
+                        id="categoria"
+                    >
+                        <option value="">-- Seleccione --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="gastos">Gastos</option>
+                        <option value="ocio">Ocio</option>
+                        <option value="salud">Salud</option>
+                        <option value="suscripciones">Suscripciones</option>
+                    </select>
+                </div>
+                <input 
+                    type="submit"
+                    value="AÑADIR GASTO"    
+                />
             </form>
         </div>
      );
